90d13c26771a5a4d604de94b449beadbe959c9a2
Cristian Beraha <cristian@cloudops.works>
2026-04-08 17:22:22 -0300

    chore: Upgrade repository from template, version: v1.6.5

--- variables-atlas.tf
+++ variables-atlas.tf
@@ -8,18 +8,20 @@
 #
 
 variable "project_id" {
-  description = "(optional) The ID of the project where the cluster will be created"
+  description = "(Optional) The ID of the MongoDB Atlas project. Mutually exclusive with project_name; provide one or the other."
   type        = string
   default     = ""
+  nullable    = true
 }
 
 variable "project_name" {
-  description = "(optional) The name of the project where the cluster will be created"
+  description = "(Optional) The name of the MongoDB Atlas project. Used to resolve the project_id when project_id is not provided. Mutually exclusive with project_id."
   type        = string
   default     = ""
+  nullable    = true
 }
 
 variable "atlas_container_id" {
-  description = "The ID of the Atlas container"
+  description = "(Required) The ID of the MongoDB Atlas network container. Used to determine the cloud provider and region for the PrivateLink endpoint."
   type        = string
 }
